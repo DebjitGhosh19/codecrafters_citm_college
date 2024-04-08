b0405a5174d8ec635c45ffa83fb7034485951610
DebjitGhosh19 <ghoshdebjit.2002@gmail.com>
2024-04-08 17:03:13 +0530

    completeproject

---
+++ script.js
@@ -0,0 +1,11 @@
+var para=document.getElementById("btn").addEventListener("click" function(){
+    for(let i=0;i<22;i++){
+        para.innerHTML=i;
+        console.log(para)
+    }
+})
+var count='';
+function add(){
+
+
+}
